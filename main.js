ba2e6f679e31b4c9e5c3da966c0de0f0dcca7a9b
Sam Considine <sambconsidine@gmail.com>
2024-04-16 11:07:03 +0100

    Updated splat

--- main.js
+++ main.js
@@ -746,8 +746,9 @@ async function main() {
     const url = new URL(
         // "nike.splat",
         // location.href,
-        params.get("url") || "train.splat",
-        "https://huggingface.co/cakewalk/splat-data/resolve/main/",
+        // params.get("url") || "train.splat",
+        // "https://huggingface.co/cakewalk/splat-data/resolve/main/",
+        "https://samconsidine.github.io/splatv1/eugene.splat",
     );
     const req = await fetch(url, {
         mode: "cors", // no-cors, *cors, same-origin
